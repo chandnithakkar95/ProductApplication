7dbfd3fa6eb3ee3a06fa951d395e87175d011712
Chandni Thakkar <chandnithakkar95@gmail.com>
2017-12-13 01:10:46 +0530

    initial Commit
    First Commit

---
+++ ProductApplication/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ProductApplication.Models;
+namespace ProductApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetProducts()
+        {
+            using (Database1Entities db =new Database1Entities())
+            {
+                var products = db.Products.OrderBy(a => a.Id).ToList();
+                return Json(new { data = products }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (Database1Entities db=new Database1Entities())
+            {
+                var p = db.Products.Where(a => a.Id == id).FirstOrDefault();
+                return View(p); 
+            }
+        }
+        [HttpPost]
+        public ActionResult Save(Product p)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (Database1Entities db = new Database1Entities())
+                {
+                    if (p.Id > 0)
+                    {
+                         var products = db.Products.Where(a => a.Id == p.Id).FirstOrDefault();
+                        if (products != null)
+                        {
+                            products.ProductName = p.ProductName;
+                            products.Price = p.Price;
+                        }
+                    }
+                    else
+                    {
+
+                        db.Products.Add(p);
+                  
+                    }
+                    db.SaveChanges();
+                    status = true;
+                }
+
+            }
+            return new JsonResult { Data = new { status=status} };
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (Database1Entities db=new Database1Entities())
+            {
+                var p = db.Products.Where(a => a.Id ==id).FirstOrDefault();
+                if (p != null)
+                {
+                    return View(p);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteProduct( int id)
+        {
+            bool status = false;
+            using (Database1Entities db=new Database1Entities())
+            {   
+                var p = db.Products.Where(a => a.Id == id).FirstOrDefault();
+                if (p != null)
+                {
+                    db.Products.Remove(p);
+                    db.SaveChanges();
+                    status = true;
+                }
+            }
+                return new JsonResult { Data = new { status = status } };
+        }
+    }
+
+}
---
+++ ProductApplication/Models/Extended/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductApplication.Models.Extended
+{
+    [MetadataType(typeof(ProductMetadata))]
+    public partial class Product
+    {
+
+    }
+    public class ProductMetadata
+    {
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Please Provide Product Name ")]
+        public string ProductName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Price")]
+        public float Price { get; set; }
+    }
+}
